5d70862daf6c464820603dbb5a78ee8d99f2ef94
ThomasTJdev <ttjarloev@gmail.com>
2018-10-27 17:38:03 +0200

    Markdown is splitted into different seq[string]'s during generating. Multiple fixes

--- tests/test1.nim
+++ tests/test1.nim
@@ -47,8 +47,14 @@ macro donothing(): untyped =
   discard"""
 
 
-test "simple":
-  check parseNimString(nimCode) == @["# My Program", "Important test", "and more", "", "# Types", "", "", "### proc markdownShow", "```nim", "proc markdownShow() =", "```", "Echo markdown"]
+test "simpleSeq":
+  check parseNimContentSeq(nimCode) == @["# My Program", "Important test", "and more", "# Types\n", "## Procs\n", "### proc markdownShow", "```nim", "proc markdownShow() =", "```", "Echo markdown"]
 
-test "complex":
-  check parseNimString(nimCodeComplex) == @["# My Program", "Important test", "and more", "", "# Types", "", "", "### proc markdownShow", "```nim", "proc markdownShow() =", "```", "Echo markdown", "My Program", "----------", "Important test", "and more", "", "Usage:", "=====", "```", "   nimtomd [options] <filename>", "", "Options:", "========", "```", "   filename.nim    File to output in Markdown", "   help            Shows the help menu", "", "```", "", "### proc globalProc*", "```nim", "proc globalProc*() =", "```", "Echo markdown", "### template nothing", "```nim", "template nothing(): string =", "```", "### macro donothing", "```nim", "macro donothing(): untyped =", "```"]
+test "complexSeq":
+  check parseNimContentSeq(nimCodeComplex) == @["# My Program", "Important test", "and more", "", "## Usage:", "```", " nimtomd [options] <filename>", "```", "", "", "## Options:", "```", " filename.nim    File to output in Markdown", " help            Shows the help menu", "", "```", "", "# Types\n", "## Procs\n", "### proc globalProc*", "```nim", "proc globalProc*() =", "```", "Echo markdown", "## Macros\n", "### template nothing", "```nim", "template nothing(): string =", "```", "### macro donothing", "```nim", "macro donothing(): untyped =", "```", "## Other\n", "### var globalVar*", "```nim", "var globalVar*: string", "```", "Important global"]
+
+test "simpleString":
+  check parseNimContentString(nimCode) == "# My Program\nImportant test\nand more\n# Types\n## Procs\n### proc markdownShow\n```nim\nproc markdownShow() =\n```\nEcho markdown\n"
+
+test "complexString":
+  check parseNimContentString(nimCodeComplex) == "# My Program\nImportant test\nand more\n\n## Usage:\n```\n nimtomd [options] <filename>\n```\n\n\n## Options:\n```\n filename.nim    File to output in Markdown\n help            Shows the help menu\n\n```\n\n# Types\n## Procs\n### proc globalProc*\n```nim\nproc globalProc*() =\n```\nEcho markdown\n## Macros\n### template nothing\n```nim\ntemplate nothing(): string =\n```\n### macro donothing\n```nim\nmacro donothing(): untyped =\n```\n## Other\n### var globalVar*\n```nim\nvar globalVar*: string\n```\nImportant global\n"
